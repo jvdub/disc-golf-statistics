584024d81e6af9eba85d796720594c5186850c89
John Van Wagenen <jvanwagenen@mastercontrol.com>
2017-11-16 22:14:25 -0700

    Fixing typo

--- src/js/round-results.js
+++ src/js/round-results.js
@@ -6,13 +6,13 @@ const table = document.getElementById('round-results');
 
 function updateResults (e) {
     let player = e.target.name.split('_')[0];
-    let playreName = table.querySelector('[name="' + player + '"').value;
+    let playerName = table.querySelector('[name="' + player + '"').value;
 
     if (!roundResults[player]) {
         roundResults[player] = new DiscGolfRound();
     }
 
-    roundResults[player].player = playreName;
+    roundResults[player].player = playerName;
     roundResults[player].scores = Array.from(table.querySelectorAll('[name^="' + player + '_hole-"]')).map(( element) => +element.value);
 
     table.querySelector('#' +player + '_total-round').innerText = roundResults[player].getTotalScore();
